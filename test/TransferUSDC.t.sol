b93cb22bf6b9cdd33864b11df107b2e3c9eaaa87
ptn77 <ctn77@protonmail.com>
2024-08-12 16:22:42 -0600

    Testing vm record logs for TransferUSDC, but there is no ccipReceive function to get gasUsage.

--- test/TransferUSDC.t.sol
+++ test/TransferUSDC.t.sol
@@ -8,106 +8,175 @@ import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.s
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {TransferUSDC} from "../src/TransferUSDC.sol";
+import {Sender} from "../src/Sender.sol";
+import {Receiver} from "../src/Receiver.sol";
 
 /// @title A test suite for TransferUSDC contract to estimate token transfers and gas usage.
 contract TransferUSDCTest is Test {
     using SafeERC20 for IERC20;
 
-    TransferUSDC public sender;
+    Sender public sender;
+    Receiver public receiver;
+    TransferUSDC public transferUSDCContract;
     BurnMintERC677 public usdcToken;
     BurnMintERC677 public link;
     MockCCIPRouter public router;
-    uint64 public chainSelector = 16015286601757825753;
     address public alice;
     address public bob;
+    uint64 public chainSelector = 16015286601757825753;
+    uint256 amountToSend; 
+
 
     /// @dev Sets up the testing environment by deploying necessary contracts and configuring their states.
     function setUp() public {
         // Initialize test addresses
         alice = makeAddr("alice");
         bob = makeAddr("bob");
+        amountToSend = 1000000; // 1 USDC (since USDC has 6 decimals)
 
         // Deploy the mock router and mock tokens (LINK and USDC)
         router = new MockCCIPRouter();
         link = new BurnMintERC677("ChainLink Token", "LINK", 18, 10**27);
-        usdcToken = new BurnMintERC677("USDC Token", "USDC", 6, 10**15); // Deploy with initial supply
-
+        usdcToken = new BurnMintERC677("USDC Token", "USDC", 6, 10**27); // Deploy with initial supply of 1,000,000 USDC
+        console.log("Deployed LINK and USDC tokens");
         // Assign initial supply of USDC to Alice
-        usdcToken.transfer(alice, usdcToken.totalSupply());
+        //usdcToken.transfer(alice, 1);
+        // Assign the minter role to the test contract address
+        link.grantMintRole(address(this));
+        usdcToken.grantMintRole(address(this));
 
+        console.log("Link address:", address(link));
+        console.log("usdcToken address:", address(usdcToken));
+        console.log("Router address:", address(router));
+
+        //transfer some link and usdc to TransferUSDC contract
+        link.mint(address(this), 6);
+        usdcToken.mint(address(this), amountToSend*2);
+
+        console.log("minted link and usdc");
         // Deploy the TransferUSDC contract with the router, LINK, and USDC addresses
-        sender = new TransferUSDC(address(router), address(link), address(usdcToken));
+        transferUSDCContract = new TransferUSDC(address(router), address(link), address(usdcToken));
 
-        // Configure the sender contract to allow transactions to the specified chain
+        // Configure the transferUSDCContract contract to allow transactions to the specified chain
+        transferUSDCContract.allowlistDestinationChain(chainSelector, true);
+
+        //Transfer Link fee tokens to the contract
+        link.approve(address(transferUSDCContract), 3);  
+        link.allowance(address(this), address(transferUSDCContract));
+        link.transfer(address(transferUSDCContract), 3);
+
+
+        console.log("Set allowance on TransferUSDC contract for spending USD on this address behalf");
+
+        usdcToken.approve(address(transferUSDCContract), amountToSend);  
+        //usdcToken.allowance(address(this), address(transferUSDCContract));
+
+
+        console.log("Balance of link in TransferUSDC contract address:", address(transferUSDCContract), link.balanceOf(address(transferUSDCContract)));
+        console.log("Balance of usdc token in this address:", address(this), usdcToken.balanceOf(address(this)));
+        console.log("Balance of usdc token in TransferUSDC contract address:", address(transferUSDCContract), usdcToken.balanceOf(address(transferUSDCContract)));
+        
+         // Sender and Receiver contracts are deployed with references to the router and LINK token.
+        sender = new Sender(address(router), address(link));
+        receiver = new Receiver(address(router));
+        // Configuring allowlist settings for testing cross-chain interactions.
         sender.allowlistDestinationChain(chainSelector, true);
+        receiver.allowlistSourceChain(chainSelector, true);
+        receiver.allowlistSender(address(sender), true);
     }
 
-    /// @dev Helper function to prepare the token transfer scenario.
-    function prepareScenario() 
-        public
-        returns (Client.EVMTokenAmount[] memory tokensToSendDetails, uint256 amountToSend) 
-    {
-        vm.startPrank(alice);
+    function bytes32ToString(
+        bytes32 _bytes32
+    ) internal pure returns (string memory) {
+        uint8 i = 0;
+        while (i < 32 && _bytes32[i] != 0) {
+            ++i;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (i = 0; i < 32 && _bytes32[i] != 0; ++i) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
 
-        amountToSend = 1000000; // 1 USDC (since USDC has 6 decimals)
-        usdcToken.approve(address(sender), amountToSend); // Alice approves TransferUSDC to spend 1 USDC
+    /// @dev Helper function to simulate sending a message from Sender to Receiver.
+    /// @param iterations The variable to simulate varying loads in the message.
+    function sendMessage(uint256 iterations) private returns (uint64 rgasUsed) {
+
+         vm.recordLogs(); // Starts recording logs to capture events.
+
+        //Not sure why the HW question is asking for the ccipReceive gas usage, since the TransferUSDC contract does not sent message.
+        transferUSDCContract.transferUsdc(
+            chainSelector,
+            bob,
+            iterations,
+            400000 // A predefined gas limit for the transaction.
+        );
+        
+        /*//Using the Sender.sol example to get the ccipReceive gas usage instead
+        sender.sendMessagePayLINK(
+            chainSelector,
+            address(receiver),
+            iterations,
+            400000 // A predefined gas limit for the transaction.
+        );*/
+
+        // Fetches recorded logs to check for specific events and their outcomes.
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+        //bytes32 msgExecutedSignature = keccak256(
+          //  "MessageExecuted(bytes32, uint64, address, bytes32)"
+        //);
+
+        for (uint i = 0; i < logs.length; i++) {
+          // if (logs[i].topics[0] == msgExecutedSignature) {
+                (bytes32 messageId,,,bytes32 result) = abi.decode(
+                    logs[i].data,
+                    (bytes32, uint64, address, bytes32)
+                );
+                console.log(
+                    "MessageExecuted event: messageId=%s, result=%s",
+                    bytes32ToString(messageId),
+                    bytes32ToString(result)
+                );
+           //}
+        }
+
+        return rgasUsed;
+    }
+
+    /// @dev Helper function to simulate the transfer and capture the gas used.
+    function transferToken() private {
+        Client.EVMTokenAmount[] memory tokensToSendDetails;
+        uint64 rgasUsed;
+        
+        rgasUsed = sendMessage(0);
+        
+        console.log("Gas used for ccipreceive: ", rgasUsed);
+        uint64 adjustedGasLimit = rgasUsed * 10/100;
+        console.log("Adjusted Gas Limit: %d", adjustedGasLimit);
 
         tokensToSendDetails = new Client.EVMTokenAmount[](1);
         Client.EVMTokenAmount memory tokenToSendDetails =
             Client.EVMTokenAmount({token: address(usdcToken), amount: amountToSend});
         tokensToSendDetails[0] = tokenToSendDetails;
 
-        vm.stopPrank();
-    }
+        uint256 balanceOfBobBefore = usdcToken.balanceOf(bob);
 
-    /// @dev Helper function to simulate the transfer and capture the gas used.
-    function transferToken() private {
-        (Client.EVMTokenAmount[] memory tokensToSendDetails, uint256 amountToSend) = prepareScenario();
+        console.log("Bob balance before transfer: ", usdcToken.balanceOf(bob));
 
-        vm.recordLogs();
-
-        uint64 initialGasLimit = 500000;
-
-        // Transfer USDC using the sender contract
-        sender.transferUsdc(
-            chainSelector,
-            bob,
-            tokensToSendDetails[0].amount,
-            initialGasLimit
-        );
-
-        Vm.Log[] memory logs = vm.getRecordedLogs();
-        bytes32 msgExecutedSignature = keccak256("MsgExecuted(bool,bytes,uint256)");
-        uint256 gasUsed;
-        for (uint i = 0; i < logs.length; i++) {
-            if (logs[i].topics[0] == msgExecutedSignature) {
-                (, , gasUsed) = abi.decode(
-                    logs[i].data,
-                    (bool, bytes, uint256)
-                );
-                console.log(
-                    "Gas used for MsgExecuted: %d",
-                    gasUsed
-                );
-            }
-        }
-
-        uint64 adjustedGasLimit = uint64((gasUsed * 110) / 100);
-        console.log("Adjusted Gas Limit: %d", adjustedGasLimit);
+        console.log("tokens to send details amount:", tokensToSendDetails[0].amount);
 
         // Re-run the transfer with the adjusted gas limit
-        sender.transferUsdc(
+        transferUSDCContract.transferUsdc(
             chainSelector,
             bob,
             tokensToSendDetails[0].amount,
             adjustedGasLimit
         );
 
-        // Verify the balances after the transfer
-        uint256 balanceOfAliceBefore = usdcToken.balanceOf(alice);
-        uint256 balanceOfBobBefore = usdcToken.balanceOf(bob);
 
-        assertEq(usdcToken.balanceOf(alice), balanceOfAliceBefore - amountToSend);
+        console.log("Bob balance after transfer: ", usdcToken.balanceOf(bob));
+        //assertEq(usdcToken.balanceOf(alice), balanceOfAliceBefore - amountToSend);
         assertEq(usdcToken.balanceOf(bob), balanceOfBobBefore + amountToSend);
     }
 
